78a58decd2196db223b051e88df1c3f84f700afc
ssimofatine <simohaji147@gmail.com>
2025-09-29 17:59:45 +0200

    //Un cliente recibe un descuento especial si se cumplen estas condiciones:         //Es miembro VIP y su compra supera 100€ O         //No es VIP pero es su cumpleaños y la compra es mayor a 50€         //En ningún caso hay descuento si el cliente tiene pagos pendientes

---
+++ src/EjercicioOperadoresLogicos2.java
@@ -0,0 +1,17 @@
+public class EjercicioOperadoresLogicos2 {
+    public static void main(String[] args) {
+
+        //Un cliente recibe un descuento especial si se cumplen estas condiciones:
+        //Es miembro VIP y su compra supera 100€ O
+        //No es VIP pero es su cumpleaños y la compra es mayor a 50€
+        //En ningún caso hay descuento si el cliente tiene pagos pendientes
+
+        boolean clienteVip;
+        double valorCompra;
+        boolean esCumpleaños;
+        boolean tienePagosPendientes;
+
+
+
+    }
+}
