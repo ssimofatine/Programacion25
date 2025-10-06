f570494aa47e6b7ea950c3f72e25333b03cba5c4
ssimofatine <simohaji147@gmail.com>
2025-10-06 17:40:02 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/EjerciciosDeIntroducciónAlaProgramación1/Ejercicio2.java
@@ -0,0 +1,17 @@
+package EjerciciosDeIntroducciónAlaProgramación1;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        /**
+         * Calcular el perímetro y área de un rectángulo dada su base y su altura.
+         */
+        double permitro, area, altura,r;
+        double PI = Math.PI;
+
+
+
+
+
+    }
+}
