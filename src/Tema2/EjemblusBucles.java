cd0f2fd5cea65f25c87ac2c113948923c0402410
ssimofatine <simohaji147@gmail.com>
2025-10-14 18:09:09 +0200

    Merge remote-tracking branch 'origin/main'

--- src/Tema2/EjemblusBucles.java
+++ src/Tema2/EjemblusBucles.java
@@ -7,10 +7,10 @@ public class EjemblusBucles {
         //2. Pide en un bucle 5 numeros por teclado y di si cada uno es par o impar
         //3. Pide por teclado cuatro notas (doble) y muestra la media de ellas
 
-        int teclado;
+        double teclado1, teclado2, teclado3, teclado4 ;
         Scanner SC = new Scanner(System.in);
 
-        teclado = SC.nextInt();
+
 
         //1.  Suma los 10 preimeros numeros (0,1,...,9) Y muestra el resultado
         int contador =0;
@@ -19,6 +19,28 @@ public class EjemblusBucles {
             suma = suma + contador;
             contador++;
         }
+
+
+        // 3. Pide por teclado cuatro notas (doble) y muestra la media de ellas
+        System.out.println("Dime numero uno: ");
+        teclado1 = SC.nextInt();
+
+        System.out.println("Dime numero dos: ");
+        teclado2 = SC.nextInt();
+
+        System.out.println("Dime numero tres: ");
+        teclado3 = SC.nextInt();
+
+        System.out.println("Dime numero cuatro: ");
+        teclado4 = SC.nextInt();
+
+        double media = (teclado1 + teclado2 + teclado3 + teclado4) / 4;
+
+        System.out.println("la media de ellas es : " + media);
+
+
+
+
 
 
 
