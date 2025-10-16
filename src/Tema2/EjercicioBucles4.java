efcffaba0d14e13d753a8e5dbdc3689c9d096d6e
ssimofatine <simohaji147@gmail.com>
2025-10-16 16:20:09 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/Tema2/EjercicioBucles4.java
@@ -0,0 +1,13 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class EjercicioBucles4 {
+    public static void main(String[] args) {
+
+        Scanner Sc = new Scanner(System.in);
+        int numTec;
+
+
+    }
+}
