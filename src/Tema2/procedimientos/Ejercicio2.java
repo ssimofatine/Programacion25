55b8969c8d5e1860b0ac8d782c924bcea4c84fa4
ssimofatine <simohaji147@gmail.com>
2025-10-23 18:55:39 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/Tema2/procedimientos/Ejercicio2.java
@@ -0,0 +1,29 @@
+package Tema2.procedimientos;
+
+public class Ejercicio2 {
+
+    public static void  pintarAsteriscos(int num){
+        for (int i = 0; i < num; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+
+        //Crear un procedimiento que pinte en la misma línea
+        //tantos asteriscos como indique un parámetro llamado num
+        //Al final que pinte un salto de línea
+
+
+        pintarAsteriscos(5);
+        pintarAsteriscos(4);
+        pintarAsteriscos(3);
+        pintarAsteriscos(2);
+        pintarAsteriscos(1);
+
+
+
+    }
+}
