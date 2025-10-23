dde4ddedd48435061275254cbb20183de527fc8b
ssimofatine <simohaji147@gmail.com>
2025-10-23 18:43:45 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/Tema2/procedimientos/test.java
@@ -0,0 +1,11 @@
+package Tema2.procedimientos;
+
+public class test {
+    public static void main(String[] args) {
+        int opciones = 5;
+        int i;
+        for (i = 1; i <= opciones - 1; i++) {
+            System.out.println("simo: " + i);
+        }
+    }
+}
