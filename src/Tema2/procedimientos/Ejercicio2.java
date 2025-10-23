ee4eb9f110c49e553eee9b4188996e5abaf4a23f
ssimofatine <simohaji147@gmail.com>
2025-10-23 18:58:07 +0200

    Merge remote-tracking branch 'origin/main'

--- src/Tema2/procedimientos/Ejercicio2.java
+++ src/Tema2/procedimientos/Ejercicio2.java
@@ -9,6 +9,13 @@ public class Ejercicio2 {
         System.out.println();
     }
 
+    public static void  pintarAsteriscosDos(int numA){
+        for (int i = 0; i < numA; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
 
@@ -22,6 +29,12 @@ public class Ejercicio2 {
         pintarAsteriscos(3);
         pintarAsteriscos(2);
         pintarAsteriscos(1);
+
+        // otro idea
+
+        for (int i = 0; i < 10; i++) {
+            pintarAsteriscosDos(i);
+        }
 
 
 
