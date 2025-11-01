e103fc9273e1f9d9788f1b0a1611ac8a44b87fbb
ssimofatine <simohaji147@gmail.com>
2025-11-02 00:22:46 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ src/Tema2/UNIDAD_2_ESTRUCTURAS_BASICAS_DE_CONTROL/Ejercicios_de_condicionales/Ejercicio2.java
@@ -0,0 +1,9 @@
+package Tema2.UNIDAD_2_ESTRUCTURAS_BASICAS_DE_CONTROL.Ejercicios_de_condicionales;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        /**
+         * Programa que lea un número por teclado e indique si es múltiplo de 2, 3, 5 o 7
+         */
+    }
+}
