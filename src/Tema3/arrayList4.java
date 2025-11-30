d0bfaf13994059c6572edb18e927d930967ab88f
ssimofatine <simohaji147@gmail.com>
2025-11-30 02:04:58 +0100

    Merge remote-tracking branch 'origin/main'

--- src/Tema3/arrayList4.java
+++ src/Tema3/arrayList4.java
@@ -2,30 +2,9 @@ package Tema3;
 
 import java.util.Scanner;
 
-public class arrayList4 {
-    public static int[] arryUno(int[] TotalAlumnos){
-
-        String[] alumno = new String[TotalAlumnos];
-
-        return TotalAlumnos2;
-
-    }
-
-    public static void NumbreDelAlumnos(){
-
-    }
-    static void main(String[] args) {
-        int numeroalumno ;
-
+class arrayList4 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dami todos total del alumnos en calse programacion:");
-        int TotalAlumnos = Integer.parseInt(sc.nextLine());
 
-        arryUno(TotalAlumnos);
-
-
-
-
-        sc.close();
     }
 }
