25f5a76350b7f93766e9f98373a8ac5524b3de55
ssimofatine <simohaji147@gmail.com>
2025-11-30 02:21:07 +0100

    Merge remote-tracking branch 'origin/main'

--- src/Tema3/arrayList4.java
+++ src/Tema3/arrayList4.java
@@ -4,7 +4,29 @@ import java.util.Scanner;
 
 class arrayList4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        double[] numeros = {3.4, 5.5, 2.9, 5.6, 7.8, 9.1, 3.3, 1.2, 4.5, 6.7, 8.4};
+
+        //Si el tamaño del array es 10 --> for va de 9 a 0
+        //Tamaño del array es --> numeros.length = 10
+        //Empiezo: 9 -> tamaño - 1
+        //Termino: 0
+        //Condicion: i >= 0
+        //Incremento: i--
+
+        for(int i=numeros.length-1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+
+        System.out.println("---------------------------");
+        //Este for y el de abajo son iguales
+        for(int i=0; i <= numeros.length-1; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        System.out.println("------------------------------");
+        for(int i=0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
 
     }
 }
